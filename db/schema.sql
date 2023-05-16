93795cf734d4a815e94014fc1fe99e9b1402faa2
JackBurke7 <128829107+JackBurke7@users.noreply.github.com>
2023-05-16 15:04:54 -0400

    Add files via upload

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS user_db;
+
+CREATE DATABASE workout_user_db
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const controllers = require('./controllers');
+const sequelize = require('./config/connection');
+const PORT = 3001;
+const app = express();
+
+app.use(controllers);
+
+
+sequelize.sync({}).then(() => {
+app.listen(PORT, () => {
+    console.log("Now Listening");
+})
+});
